83783c500c31cd80f52c06f859b06fdfe6190591
SaadYum <saadrehman415@gmail.com>
2022-01-31 22:48:33 +0500

    Events API setupand related Mock Tests added and covered

--- utils/__tests__/api.test.js
+++ utils/__tests__/api.test.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 
 const MockAdapter = require("axios-mock-adapter");
-const { searchArtistAPI } = require("../api");
+const { searchArtistAPI, getArtistEventsAPI } = require("../api");
 
 describe("searchArtist", () => {
   let mock;
@@ -39,6 +39,48 @@ describe("searchArtist", () => {
 
       searchArtistAPI("atif%20a").then((response) => {
         expect(response).toEqual(null);
+        done();
+      });
+    });
+  });
+});
+
+describe("getEvents", () => {
+  let mock;
+
+  beforeAll(() => {
+    mock = new MockAdapter(axios);
+  });
+
+  afterEach(() => {
+    mock.reset();
+  });
+  describe("when getArtistEventsAPI call is successful", () => {
+    it("should return events data for a provided artist", (done) => {
+      const data = { response: true };
+      mock
+        .onGet(
+          `https://rest.bandsintown.com/artists/chris/events?app_id=${process.env.NEXT_PUBLIC_APP_ID}`
+        )
+        .reply(200, data);
+
+      getArtistEventsAPI("chris").then((response) => {
+        expect(response).toEqual(data);
+        done();
+      });
+    });
+  });
+
+  describe("when getArtistEventsAPI call is unsuccessful", () => {
+    it("should return empty list of events", (done) => {
+      mock
+        .onGet(
+          `https://rest.bandsintown.com/artists//events?app_id=${process.env.NEXT_PUBLIC_APP_ID}`
+        )
+        .networkErrorOnce();
+
+      getArtistEventsAPI("").then((response) => {
+        expect(response).toEqual([]);
         done();
       });
     });
